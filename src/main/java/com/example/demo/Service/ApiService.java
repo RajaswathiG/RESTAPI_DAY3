3534ffa14262875da8ab8e7a8c7ba375578ff5dc
RajaswathiG <727721euec119@skcet.ac.in>
2023-08-23 09:10:26 +0530

    first commit

---
+++ src/main/java/com/example/demo/Service/ApiService.java
@@ -0,0 +1,33 @@
+package com.example.demo.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import com.example.demo.Model.Employee;
+import com.example.demo.repository.EmployeeRepo;
+
+@Service
+public class ApiService {
+	@Autowired
+	EmployeeRepo er;
+	public Employee saveinfo(Employee ee)
+	{
+		return er.save(ee);
+	}
+	
+	public List<Employee> showinfo(){
+		return er.findAll();
+	}
+
+	public Optional<Employee> getEmployeeById(int id) {
+		return er.findById(id);
+	}
+	public Optional<Employee> getEmpid(int id) {
+		return er.findById(id);
+	}
+	
+}
---
+++ src/main/java/com/example/demo/controller/ApiController.java
@@ -0,0 +1,44 @@
+package com.example.demo.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Model.Employee;
+import com.example.demo.Service.ApiService;
+
+@RestController
+public class ApiController {
+
+	
+	@Autowired
+	ApiService aser;
+	
+	@PostMapping("/")
+	public boolean add(@RequestBody Employee ee)
+	{
+	    return aser.saveinfo(ee)!=null;
+	}
+	@GetMapping("/")
+	public List<Employee> show()
+	{
+		return aser.showinfo();
+	}
+	@GetMapping("/{id}")
+	public Optional<Employee> oneEmployee(@PathVariable int id) {
+		return aser.getEmployeeById(id);
+	}
+	@GetMapping("getid")
+	public Optional<Employee> oneEmp(@RequestParam int id) {
+		return aser.getEmployeeById(id);
+	}
+	
+
+}
